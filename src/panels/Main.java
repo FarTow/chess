e390bc873381a3ae874df257c578f1b3c38ffd66
FarTow <farhid1@live.com>
2020-06-15 19:18:29 -0500

    Made the StartMenu a local variable

--- src/panels/Main.java
+++ src/panels/Main.java
@@ -1,7 +1,5 @@
 package panels;
 
-// Ignore this
-
 import javax.swing.JFrame;
 
 import java.awt.CardLayout;
@@ -11,12 +9,11 @@ public class Main extends JFrame {
     static final String GAME_LABEL = "GAME";
 
     private final CardLayout cards;
-    private final StartMenu startMenu;
     private final Game game;
 
     Main() {
         cards = new CardLayout();
-        startMenu = new StartMenu(this);
+        StartMenu startMenu = new StartMenu(this);
         game = new Game();
 
         setLayout(cards);
