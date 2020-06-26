ec59eb5cbfc8656ecc56a65995a2c1971d882acb
FarTow <farhid1@live.com>
2020-06-25 22:51:56 -0500

    Formatted eastDisplay

--- src/panels/Game.java
+++ src/panels/Game.java
@@ -56,13 +56,15 @@ public class Game extends JPanel implements ActionListener {
         eastDisplay.setLayout(new BoxLayout(eastDisplay, BoxLayout.Y_AXIS));
 
         board = new Board(new Point(10,getHeight()*2/5 - 240));
-        moveHistory = new MoveHistory(new Point(0, 0), new Dimension(getWidth()/3, getHeight()/5), board);
+        moveHistory = new MoveHistory(new Point(0, 0), new Dimension(getWidth()/3, getHeight()*2/5), board);
         blackTakenPieces = new TakenPieces(new Point(0, 0), true, board);
         whiteTakenPieces = new TakenPieces(new Point(0, 0), false, board);
 
-        Main.forceSize(new Dimension(getWidth()/3, getHeight()/5), blackTakenPieces, whiteTakenPieces);
+        Main.forceSize(new Dimension(getWidth()/3, getHeight()/10), blackTakenPieces, whiteTakenPieces);
 
         centerDisplay.add(board);
+        eastDisplay.setBackground(new Color(194, 194, 194));
+        eastDisplay.add(Box.createRigidArea(new Dimension(getWidth()/3, getHeight()/10)));
         eastDisplay.add(blackTakenPieces);
         eastDisplay.add(moveHistory);
         eastDisplay.add(whiteTakenPieces);
