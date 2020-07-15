0e8e5fe2527b5fcdc5fb15befcc8174f58844693
FarTow <farhid1@live.com>
2020-07-15 14:21:47 -0500

    Prettier buttons

--- src/panels/StartMenu.java
+++ src/panels/StartMenu.java
@@ -19,12 +19,15 @@ public class StartMenu extends JPanel {
         add(Box.createRigidArea(new Dimension(main.getWidth(), main.getHeight()/3)));
         add(startButton);
         add(Box.createRigidArea(new Dimension(main.getWidth(), main.getHeight()/20)));
-        add(new JButton("Test"));
+        add(new JButton("Set Time Mode"));
 
         for (int i=0; i<getComponentCount(); i++) {
             if (!(getComponent(i) instanceof JButton)) {
                 continue;
             }
+
+            Main.forceSize(new Dimension(main.getWidth()/6, main.getHeight()/20), getComponent(i));
+            getComponent(i).setFont(new Font("Serif", Font.PLAIN, 20));
 
             ((JButton) getComponent(i)).setAlignmentX(Component.CENTER_ALIGNMENT);
             getComponent(i).setFocusable(false);
