aab66496907ef86156d82e6222dc2031d0ebe33a
FarTow <farhid1@live.com>
2020-07-19 12:48:40 -0500

    Separator line between time and piece difference

--- src/panels/TimeDisplay.java
+++ src/panels/TimeDisplay.java
@@ -22,10 +22,14 @@ public class TimeDisplay extends JPanel {
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setFont(Main.MULISH_LIGHT.deriveFont(Math.min((float) getWidth()/2, (float) getHeight()/2)));
 
+        g2d.setColor(Color.gray);
+        g2d.drawLine(getWidth()/25, getHeight()*9/10, getWidth()*24/25, getHeight()*9/10);
+
         Dimension timeDimensions = new Dimension(
                 g2d.getFontMetrics(g2d.getFont()).stringWidth("2:30"),
                 g2d.getFontMetrics(g2d.getFont()).getHeight()
         );
+        g2d.setColor(Color.black);
         g2d.drawString("2:30", getWidth()/2 - timeDimensions.width/2, getHeight()/2 + timeDimensions.height/4);
     }
     public void actionPerformed(ActionEvent ae) {
