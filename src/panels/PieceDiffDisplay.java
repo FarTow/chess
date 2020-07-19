5968f864094fc2934850bb3d3f39c859ae95ec2a
FarTow <farhid1@live.com>
2020-07-19 12:36:46 -0500

    Center counts

--- src/panels/PieceDiffDisplay.java
+++ src/panels/PieceDiffDisplay.java
@@ -10,7 +10,7 @@ public class PieceDiffDisplay extends JPanel {
     private final Image[] pieceImages;
     private final Player player;
 
-    private int[] advantageCount;
+    private int[] diffCount;
     private final boolean isWhite;
 
     public PieceDiffDisplay(Player player) {
@@ -18,9 +18,9 @@ public class PieceDiffDisplay extends JPanel {
         this.player = player;
         this.isWhite = player.isWhite();
         pieceImages = new Image[5];
-        advantageCount = new int[5];
+        diffCount = new int[5];
 
-        updateAdvantageCount();
+        updateDiffCount();
 
         pieceImages[0] = Main.greyPieceImages[Main.PAWN_IMAGE_INDEX];
         pieceImages[1] = Main.greyPieceImages[Main.KNIGHT_IMAGE_INDEX];
@@ -29,14 +29,14 @@ public class PieceDiffDisplay extends JPanel {
         pieceImages[4] = Main.greyPieceImages[Main.QUEEN_IMAGE_INDEX];
     }
 
-    protected void updateAdvantageCount() {
+    protected void updateDiffCount() {
         if (player.getPieces().size() == player.getEnemyPlayer().getPieces().size()) return;
 
-        advantageCount[0] = player.getPieceCount()[0]-player.getEnemyPlayer().getPieceCount()[0];
-        advantageCount[1] = player.getPieceCount()[1]-player.getEnemyPlayer().getPieceCount()[1];
-        advantageCount[2] = player.getPieceCount()[2]-player.getEnemyPlayer().getPieceCount()[2];
-        advantageCount[3] = player.getPieceCount()[3]-player.getEnemyPlayer().getPieceCount()[3];
-        advantageCount[4] = player.getPieceCount()[4]-player.getEnemyPlayer().getPieceCount()[4];
+        diffCount[0] = player.getPieceCount()[0]-player.getEnemyPlayer().getPieceCount()[0];
+        diffCount[1] = player.getPieceCount()[1]-player.getEnemyPlayer().getPieceCount()[1];
+        diffCount[2] = player.getPieceCount()[2]-player.getEnemyPlayer().getPieceCount()[2];
+        diffCount[3] = player.getPieceCount()[3]-player.getEnemyPlayer().getPieceCount()[3];
+        diffCount[4] = player.getPieceCount()[4]-player.getEnemyPlayer().getPieceCount()[4];
 
     }
 
@@ -45,28 +45,27 @@ public class PieceDiffDisplay extends JPanel {
 
         int rowModifier = -2;
         for (int i=0; i<pieceImages.length; i++, rowModifier++) {
-
             int pieceSpacingOffset = rowModifier*pieceImages[i].getWidth(null);
+            Point pieceImageTopLeft = new Point((getWidth()/2 - pieceImages[i].getWidth(null)/2) + pieceSpacingOffset, 0);
 
-            g.drawImage(
-                    pieceImages[i],
-                    (getWidth()/2 - pieceImages[i].getWidth(null)/2) + pieceSpacingOffset,
-                    0,
-                    null
-            );
+            // Draw image at specified location
+            g.drawImage(pieceImages[i], pieceImageTopLeft.x, pieceImageTopLeft.y, null);
 
+            // Edit graphics as desired
             Graphics2D g2d = (Graphics2D) g;
-            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // sharper text
 
-            g2d.setColor(Color.black);
-            g2d.setFont(Main.MULISH_LIGHT.deriveFont(Math.min((float) getWidth()/30, (float) getHeight()/20)));
+            g2d.setColor(Color.black); // black text
+            g2d.setFont(Main.MULISH_LIGHT.deriveFont(Math.min((float) getWidth()/15, (float) getHeight()/15))); // font
 
+            // Draw the piece difference
             String count;
-            if (advantageCount[i] == 0) {
+            if (diffCount[i] == 0) {
                 count = "-";
             } else {
-                count = Integer.toString(advantageCount[i]);
+                count = Integer.toString(diffCount[i]);
             }
+
             Dimension countDimensions = new Dimension(
                     g2d.getFontMetrics(g.getFont()).stringWidth(count),
                     g2d.getFontMetrics(g.getFont()).getHeight()
@@ -74,13 +73,13 @@ public class PieceDiffDisplay extends JPanel {
 
             g2d.drawString(
                     count,
-                    (getWidth()/2 - pieceImages[i].getWidth(null)/2 + countDimensions.width*7/2) + pieceSpacingOffset,
-                    pieceImages[i].getHeight(null)+countDimensions.height
+                    pieceImageTopLeft.x + pieceImages[i].getWidth(null)/2 - countDimensions.width/2,
+                    pieceImageTopLeft.y + pieceImages[i].getHeight(null)*5/4
             );
         }
     }
     public void actionPerformed(ActionEvent ae){
-        updateAdvantageCount();
+        updateDiffCount();
         repaint();
     }
 }
