a65a779a989b6fcc8bc27a41a16be12e8859aa02
galpticose <farhid1@live.com>
2020-10-07 11:10:36 -0500

    Removed comment

--- src/entities/Pawn.java
+++ src/entities/Pawn.java
@@ -45,54 +45,6 @@ public class Pawn extends Piece {
         return false;
     }
 
-    /*
-    public void update(Board board) {
-        moveableSquares = new ArrayList<>();
-        Square[][] grid = board.getGrid();
-
-        for (Square[] squareRow : grid) {
-            for (Square square : squareRow) {
-                int newRow = square.getRow();
-                int newCol = square.getCol();
-                int movementModifier = isWhite ? -1 : 1;
-
-                if (!jumping(newRow, newCol, grid) && !(Math.abs(getCol()-newCol) > 1)) {
-                    if (square.getPiece() == null) { // moving to a spot with no piece
-                        if (getCol() == newCol) { // moving to a spot in the same column
-                            if (firstMove) { // move two on first turn
-                                if (getRow() + movementModifier * 2 == newRow) {
-                                    moveableSquares.add(square);
-                                }
-                            }
-                            if (getRow() + movementModifier == newRow) moveableSquares.add(square);
-                        } else { // when moving to a spot with no piece, only other case is en passant
-                            if (Math.abs(getRow() - newRow) == 1) {
-                                if ((newRow + movementModifier * -1 >= 0 && newRow + movementModifier * -1 <= 7)) {
-                                    Square pawnSquare = grid[newRow + movementModifier * -1][newCol];
-
-                                    if (pawnSquare.getPiece() instanceof Pawn) {
-                                        if (isWhite != pawnSquare.getPiece().isWhite) {
-                                            if (((Pawn) pawnSquare.getPiece()).isEnPassantCapturable()) {
-                                                moveableSquares.add(square);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    } else { // moving to a spot with a piece
-                        if (getCol() != newCol) { // moving to a spot on a different column
-                            if (isWhite != square.getPiece().isWhite()) { // the piece is a different color
-                                if (getRow() - newRow == movementModifier * -1) moveableSquares.add(square);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    */
-
     public void setEnPassantCapturable(boolean enPassantCapturable) { this.enPassantCapturable = enPassantCapturable; }
 
     public boolean isEnPassantCapturable() { return enPassantCapturable; }
