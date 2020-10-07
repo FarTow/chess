888d59bd534fec41fcc56aaeebb9932dd2718bcd
galpticose <farhid1@live.com>
2020-10-07 11:10:04 -0500

    New canMove method

--- src/entities/Pawn.java
+++ src/entities/Pawn.java
@@ -14,6 +14,38 @@ public class Pawn extends Piece {
         setImage();
     }
 
+    @Override
+    public boolean canMove(int newRow, int newCol, Square[][] grid) {
+        int movementModifier = isWhite ? -1 : 1;
+
+        if (!jumping(newRow, newCol, grid) && !(Math.abs(getCol() - newCol) > 1)) {
+            if (grid[newRow][newCol].getPiece() == null) { // move to empty square on same col
+                if (getCol() == newCol) {
+                    if (firstMove) { // move 2
+                        return getRow() + movementModifier * 2 == newRow || getRow() + movementModifier == newRow;
+                    }
+                    return getRow() + movementModifier == newRow;
+                } else { // en passant
+                    if (Math.abs(getRow() - newRow) == 1 &&
+                            (newRow + movementModifier * -1 >= 0 && newRow + movementModifier * -1 <= 7)) {
+                        Square pawnSquare = grid[newRow + movementModifier * -1][newCol];
+
+                        if (pawnSquare.getPiece() instanceof Pawn && isWhite != pawnSquare.getPiece().isWhite) {
+                            return ((Pawn) pawnSquare.getPiece()).isEnPassantCapturable();
+                        }
+                    }
+                }
+            } else { // move to taken square
+                if (getCol() != newCol && isWhite != grid[newRow][newCol].getPiece().isWhite()) {
+                    return getRow() - newRow == movementModifier * -1;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /*
     public void update(Board board) {
         moveableSquares = new ArrayList<>();
         Square[][] grid = board.getGrid();
@@ -59,6 +91,7 @@ public class Pawn extends Piece {
             }
         }
     }
+    */
 
     public void setEnPassantCapturable(boolean enPassantCapturable) { this.enPassantCapturable = enPassantCapturable; }
 
